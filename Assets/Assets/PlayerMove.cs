dc7a53fa944ee682791df5c0ffad908467f1d617
yuu <hornet9999q@yahoo.co.jp>
2018-12-16 11:27:30 +0900

    CarGame

---
+++ Assets/Assets/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(-1*speed*Time.deltaTime, 0f, 0f);
+        if (Input.GetMouseButton(0)){
+            transform.position += new Vector3(+1 * speed * Time.deltaTime, 0f,-1*speed*Time.deltaTime);
+
+
+        }
+	}
+}
